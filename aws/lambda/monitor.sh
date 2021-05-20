3aeac68fe1edea2f4566cb03239401494d91e642
Genghis Yang <yangcheng2503@163.com>
2021-05-20 22:41:12 +0800

    Fix invoking executable binary

--- aws/lambda/monitor.sh
+++ aws/lambda/monitor.sh
@@ -1,6 +1,6 @@
 function handler () {
   EVENT_DATA=$1
-  bash ./gGiftNotifier
+  bash -c "./gGiftNotifier"
   RESPONSE="{\"statusCode\": 200, \"body\": \"Run monitoring successfully!\"}"
   echo $RESPONSE
 }
